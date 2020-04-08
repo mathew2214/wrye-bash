6d21302515f66a3db00aff82b27d52a6505c37aa
Infernio <infernio@icloud.com>
2020-04-08 03:42:09 +0200

    Interlude, pt3: defaultEnabled -> default_enabled...
    ...and make it static

--- Mopy/bash/game/__init__.py
+++ Mopy/bash/game/__init__.py
@@ -383,7 +383,7 @@ class GameInfo(object):
 
     To make a tweak Enabled by Default, enclose the tuple entry for the
     tweak in a list, and make a dictionary as the second list item with {
-    'defaultEnabled ':True}. See the UOP Vampire face fix for an example of
+    u'default_enabled': True}. See the UOP Vampire face fix for an example of
     this (in the GMST Tweaks)
     """
     GlobalsTweaks = []
@@ -406,7 +406,7 @@ class GameInfo(object):
 
     To make a tweak Enabled by Default, enclose the tuple entry for the
     tweak in a list, and make a dictionary as the second list item with {
-    'defaultEnabled ':True}. See the UOP Vampire facefix for an example of
+    u'default_enabled': True}. See the UOP Vampire facefix for an example of
     this (in the GMST Tweaks)
     """
     GmstTweaks = []
